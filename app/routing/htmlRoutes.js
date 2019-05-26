e544740e2cd94ab2196de485c25de278161f1a69
Sergio Barrera <sdbeng@gmail.com>
2019-05-26 12:45:58 -0700

    Testing routes - not best practices

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,11 +1,40 @@
+const express = require("express")
 const path = require("path")
+const friends = require("../data/friends")
+const app = express()
+// module.exports = (app) => {
+//     app.get("/", (req,res) => {
+//         res.sendFile(path.join(__dirname, "/../public/home.html"))
+//     })
 
-module.exports = (app) => {
-    app.get("/", (req,res) => {
-        res.sendFile(path.join(__dirname, "/../public/home.html"))
-    })
+//     app.get("/survey", (req, res) => {
+//         res.sendFile(path.join(__dirname, "/../public/survey.html"))
+//     })
+// }
 
-    app.get("/survey", (req, res) => {
-        res.sendFile(path.join(__dirname, "/../public/survey.html"))
-    })
+const homeView = app => {
+  app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/home.html"))
+  })
+}
+
+const hack = app => {
+  app.get("/hack", (req, res) => {
+    console.log("hacking...")
+    const dani = { name: "Dani" }
+
+    // res.json(dani)
+    res.json(friends)
+  })
+}
+
+const funcTest = () => {
+  //   console.log("funcTest LOG")
+  return "Hey yooo!"
+}
+
+module.exports = {
+  homeView: homeView,
+  funcTest: funcTest,
+  hack
 }
--- index.js
+++ index.js
@@ -1,18 +1,32 @@
 const express = require("express")
 const path = require("path")
-
+const home = require("./app/routing/htmlRoutes")
 
 const app = express()
 let PORT = process.env.PORT || 8089
 
 /////////////////////////////////////////////
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
 /////////////////////////////////////////////
 // router - require routes
 /////////////////////////////////////////////
-require("./app/routing/htmlRoutes")(app)
+// require("./app/routing/htmlRoutes")(app)
+
+/////////////////////////////////////////////
+// test2
+/////////////////////////////////////////////
+const hey = home.funcTest()
+console.log(hey)
+/////////////////////////////////////////////
+// testing root /
+/////////////////////////////////////////////
+// NOTE: if playing with 2 app instances you must pass app as a param to the func
+home.homeView(app)
+
+// hack
+home.hack(app)
 
 /////////////////////////////////////////////
 // test
@@ -26,6 +40,6 @@ require("./app/routing/htmlRoutes")(app)
 
 /////////////////////////////////////////////
 // server listen
-app.listen(PORT, function(){
-    console.log("App listening on port", PORT)
+app.listen(PORT, function() {
+  console.log("App listening on port", PORT)
 })
